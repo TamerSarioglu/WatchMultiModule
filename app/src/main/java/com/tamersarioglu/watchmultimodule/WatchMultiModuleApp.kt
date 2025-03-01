312749facd96b4a564b6dc53a083b5d74b61990f
Tamer Sarioglu <tamerthedark@hotmail.com>
2025-03-01 04:12:22 +0300

    add Hilt support and application class for dependency injection

---
+++ app/src/main/java/com/tamersarioglu/watchmultimodule/WatchMultiModuleApp.kt
@@ -0,0 +1,8 @@
+package com.tamersarioglu.watchmultimodule
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WatchMultiModuleApp: Application() {
+}

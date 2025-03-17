069c6ff5b1ac97ad7897eb0e91e0f79c0d66d713
Tamer Sarioglu <tamerthedark@hotmail.com>
2025-03-18 01:23:11 +0300

    chore: clean up and organize build configuration files

--- build-logic/convention/src/main/kotlin/AndroidComposeConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidComposeConventionPlugin.kt
@@ -1,18 +1,11 @@
 import com.android.build.api.dsl.CommonExtension
+import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
 import utils.libs
-import com.android.build.gradle.LibraryExtension
-import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
-import com.android.build.api.dsl.BuildFeatures
-import com.android.build.api.dsl.BuildType
-import com.android.build.api.dsl.DefaultConfig
-import com.android.build.api.dsl.ProductFlavor
-import com.android.build.api.dsl.AndroidResources
-import com.android.build.api.dsl.Installation
 
 class AndroidComposeConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -23,7 +16,7 @@ class AndroidComposeConventionPlugin : Plugin<Project> {
                     configureCompose(this)
                 }
             }
-            
+
             pluginManager.withPlugin("com.android.library") {
                 extensions.configure<LibraryExtension> {
                     configureCompose(this)
@@ -34,16 +27,19 @@ class AndroidComposeConventionPlugin : Plugin<Project> {
                 val bom = libs.findLibrary("androidx-compose-bom").get()
                 add("implementation", platform(bom))
                 add("androidTestImplementation", platform(bom))
-                
+
                 // Add compose dependencies
                 add("implementation", libs.findLibrary("androidx.activity.compose").get())
                 add("implementation", libs.findLibrary("androidx.lifecycle.runtime").get())
                 add("implementation", libs.findLibrary("androidx.lifecycle.viewmodel").get())
                 add("implementation", libs.findLibrary("androidx.lifecycle.compose").get())
-                
+
                 add("debugImplementation", libs.findLibrary("androidx.compose.ui.tooling").get())
                 add("androidTestImplementation", libs.findLibrary("androidx.compose.ui.test").get())
-                add("debugImplementation", libs.findLibrary("androidx.compose.ui.test.manifest").get())
+                add(
+                    "debugImplementation",
+                    libs.findLibrary("androidx.compose.ui.test.manifest").get()
+                )
 
                 // Add compose bundle
                 libs.findBundle("compose").get().get().forEach { dependency ->
@@ -62,7 +58,8 @@ class AndroidComposeConventionPlugin : Plugin<Project> {
             }
 
             composeOptions {
-                kotlinCompilerExtensionVersion = libs.findVersion("compose.compiler").get().toString()
+                kotlinCompilerExtensionVersion =
+                    libs.findVersion("compose.compiler").get().toString()
             }
         }
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -6,5 +8,3 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
 }
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
